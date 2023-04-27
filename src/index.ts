01993a6b23369e165817681cb2f39c2a0d1d41f9
MaritaIfemeni <marita.ifemeni@gmail.com>
2023-04-27 15:31:09 +0300

    practising API fetch

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,61 @@
+import axios from "axios";
 
-class Test {
-    name: string
-    constructor(name: string) {
-        this.name = "testi nimi"
-    }
+
+interface Company {
+    businessId: string;
+    name: string;
 }
 
-console.log(Test.name)
+
+
+const getCompanies = async () => {
+  try {
+    const response = await axios.get(
+      "http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=3&resultsFrom=0&companyRegistrationFrom=2014-02-28"
+    );
+    const companies: Company[] = response.data.results.map((result: any) => ({
+        businessId: result.businessId,
+        name: result.name,
+        }));
+    return companies;
+  } catch (error) {
+    console.log(error + "error fetching companies");
+  }
+};
+
+
+
+const getCompanyDetails = async (businessId: string) => {
+  try {
+    const response = await axios.get(
+      `http://avoindata.prh.fi/opendata/bis/v1/${businessId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error + "error fetching company details");
+  }
+};
+
+getCompanies().then((data) => {
+  console.log(data);
+});
+
+getCompanyDetails("3361252-1").then((data) => {
+  console.log(data);
+});
+
+
+
+// http://avoindata.prh.fi/opendata/bis/v1/{businessId}
+
+//GET /BIS/V1
+// http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=3&resultsFrom=0&companyRegistrationFrom=2014-02-28
+
+// BisCompany {
+//     businessId (string): Business ID ,
+//     name (string): Primary company name
+//     registrationDate (string): Date of registration ,
+//     companyForm (string, optional): Company form ,
+//     detailsUri (string, optional): A URI for more details, if details aren't already included ,
+//     liquidations (Array[BisCompanyLiquidation], optional): Bankruptcy, liquidation or restructuring proceedings ,
+//     }
