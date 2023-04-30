9fbf5f3f3f709effc2dc74fb7c7853f437eebb10
MaritaIfemeni <marita.ifemeni@gmail.com>
2023-04-30 22:49:11 +0300

    finalizing code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import axios from "axios";
-import { BisCompany, BisCompanyDetails } from "./types/businessTypes";
+import { BisCompanyDetails } from "./types/businessTypes";
 
 const getCompanies = async (
   maxResult: number,
   resultFrom: number,
   postCode: string
-) => {
+): Promise<Array<[string, BisCompanyDetails]>> => {
   try {
     const response = await axios.get(
       `http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${maxResult}&resultsFrom=${resultFrom}&streetAddressPostCode=${postCode}`
     );
-    console.log("List of IDs:");
-    return response.data.results[0].businessId;
+    const businesses = response.data.results;
+    const companiesDetails = [];
+    for (let i = 0; i < businesses.length; i++) {
+      const businessId = businesses[i].businessId;
+      const companyDetails = await getCompanyDetails(businessId);
+      companiesDetails.push([businessId, companyDetails]);
+    }
+    return companiesDetails as Array<[string, BisCompanyDetails]>;
   } catch (error) {
-    console.log(error + "error fetching companies");
+    console.log(error + "error fetching companies details");
+    return [];
   }
 };
 
@@ -22,9 +29,7 @@ const getCompanyDetails = async (businessId: string) => {
     const response = await axios.get(
       `http://avoindata.prh.fi/opendata/bis/v1/${businessId}`
     );
-
     const items = response.data.results[0] as BisCompanyDetails;
-    console.log("List of Company details:");
     return items;
   } catch (error) {
     console.log(error + "error fetching company details");
@@ -32,14 +37,25 @@ const getCompanyDetails = async (businessId: string) => {
 };
 
 const showListOfIds = async () => {
-  const result = await getCompanies(2, 0, "01730");
-  console.log(result);
+  const result = await getCompanies(4, 0, "01730");
+  console.log(
+    "List of Companies with details based on the params: maxResult, resultFrom and postCode:"
+  );
+  result.forEach((elem) => {
+    if (Array.isArray(elem)) {
+      elem.forEach((nestedElem) => {
+        console.log(nestedElem);
+      });
+    } else {
+      console.log(elem);
+    }
+  });
 };
 
-const showCompanyDetail = async () => {
-  const result = await getCompanyDetails("3355613-6");
-  console.log(result);
-};
+// const showCompanyDetail = async () => {
+//   const result = await getCompanyDetails("3355613-6");
+//   console.log(result);
+// };
+//showCompanyDetail();
 
-showCompanyDetail();
 showListOfIds();
